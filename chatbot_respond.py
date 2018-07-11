44515ea44c67cbc07412aa44eb6606db89fc704c
FUNS21 <39723057+FUNS21@users.noreply.github.com>
2018-07-11 23:02:53 +0800

    sugar levels response
    doesnt consider values like 6.8

--- chatbot_respond.py
+++ chatbot_respond.py
@@ -1,86 +1,97 @@
 import nltk, string, random
 
 #initialise
-user_greet=input("Welcome to MEDIBot! Start with a greeting, or cancel: ")
-user_respond="I want to view medical records."
+user_respond="I got a reading of 0."
 
 #noise reduction/expression removal like "lah" and punctuation removal
-lst_stop_words=open("stop_words_and_singlish.txt", "r")
-stop_words=[]
-for line in lst_stop_words:
-    stop_words.append(''.join(line.strip().split("\n")))
-lst_stop_words.close()
+def preprocessing(input_text):
+    lst_stop_words=open("stop_words_and_singlish.txt", "r")
+    stop_words=[]
+    for line in lst_stop_words:
+        stop_words.append(''.join(line.strip().split("\n")))
+    lst_stop_words.close()
 
-def _remove_noise(input_text):
-    words = input_text.split() 
-    noise_free_words = [word for word in words if word not in stop_words] 
-    noise_free_text = " ".join(noise_free_words)
-    translator_punc=str.maketrans('','', string.punctuation)
-    noise_free_text=noise_free_text.translate(translator_punc)
-    return noise_free_text
+    def _remove_noise(input_text):
+        words = input_text.split() 
+        noise_free_words = [word for word in words if word not in stop_words] 
+        noise_free_text = " ".join(noise_free_words)
+        translator_punc=str.maketrans('','', string.punctuation)
+        noise_free_text=noise_free_text.translate(translator_punc)
+        return noise_free_text
 
-words=(_remove_noise(user_greet))
+    words=(_remove_noise(input_text))
 
-#word standardisation:
-phrases=open("singlish_phrases.txt", "r")
-singlish_phrases=[]
-for line in phrases:
-    singlish_phrases.append(line.strip("\n").split(","))
-phrases.close()
-dic_singlish={x[0]:x[1] for x in singlish_phrases}
+    #word standardisation:
+    phrases=open("singlish_phrases.txt", "r")
+    singlish_phrases=[]
+    for line in phrases:
+        singlish_phrases.append(line.strip("\n").split(","))
+    phrases.close()
+    dic_singlish={x[0]:x[1] for x in singlish_phrases}
 
-def _lookup_words(input_text):
-    words = input_text.split() 
-    new_words = [] 
-    for word in words:
-        if word.lower() in dic_singlish:
-            word = dic_singlish[word.lower()]
-        new_words.append(word)
-    new_text = " ".join(new_words) 
-    return new_text
+    def _lookup_words(input_text):
+        words = input_text.split() 
+        new_words = [] 
+        for word in words:
+            if word.lower() in dic_singlish:
+                word = dic_singlish[word.lower()]
+            new_words.append(word)
+        new_text = " ".join(new_words) 
+        return new_text
 
-new_words=_lookup_words(words)
+    new_words=_lookup_words(words)
 
-#lemmtising and stemming 
-from nltk.stem.wordnet import WordNetLemmatizer
-from nltk.stem.porter import PorterStemmer
-lem = WordNetLemmatizer()
-stem = PorterStemmer()
+    #lemmtising and stemming 
+    from nltk.stem.wordnet import WordNetLemmatizer
+    from nltk.stem.porter import PorterStemmer
+    lem = WordNetLemmatizer()
+    stem = PorterStemmer()
 
-word_result=[]
-for word in new_words:
-    temp=lem.lemmatize(word)
-    word_result.append(temp)
+    word_result=[]
+    for word in new_words:
+        temp=lem.lemmatize(word)
+        word_result.append(temp)
 
-new_words= ''.join(word_result)
+    new_words= ''.join(word_result)
+    return new_words
 
 #greeting check? 
-def response_greetings(greeting):
-    greeting_file=open("Greetings.txt", "r")
-    lst_greet=[]
-    for line in greeting_file:
-        lst_greet.append((''.join(line.strip('\n').split('\n'))))
-    greeting_file.close()
-    greeting=list(greeting.split())
+def greeting_check(input_text):
+    input_text = preprocessing(input_text) 
+    greeting=list(input_text.split())
     for i in range(len(greeting)):
-        if greeting[i].lower() in ['hello', 'hi', 'hey', 'sup']:
-            return random.choice(lst_greet)
-        else:
-            return ("Whoops, try a greeting instead!")
+        if greeting[i].lower() in ['hello', 'hi', 'hey', 'supp']:
+            return True
+    return False
 
-print(response_greetings(new_words))
+def medical_check(input_text):
+    input_text = preprocessing(input_text)
+    input_text=list(input_text.split())
+    for i in range(len(input_text)):
+        if input_text[i].lower() in ['medical', 'records', 'record']:
+            return True
+    return False
 
-#medical response
-def response(user_response):
-    user_response=list(user_response.split())
-    for i in range(len(user_response)):
-        if user_response[i].lower() in ['medical', 'record', 'records']:
-            return "Your current records: " + "RECORDS"
-        elif user_response[i].lower() in ['sugar', 'diabetic']:
-            return "Your current status for DIABETES is: " + "diabetic/non" + "thus, SUGAR is " + "no/moderation"
-    return "Anything else?"
 
-responses=_remove_noise(user_respond)
-print(response(responses))
+def check_blood_sugar(input_text):
+    input_text=preprocessing(input_text)
+    values=[]
+    for i in range(len(input_text)):
+        try:
+            value=float(input_text[i]) #loses all precision?
+            values.append(value)
+        except:
+            pass
+        for j in range(len(values)):
+            value=values[j]
+            if  value > 6.0 and  value < 4.0:
+                return "You are at: MEDIUM risk. Consult a medical professional."
+            elif value>=11.0 or value<=2.8:
+                return "You are at: HIGH risk. Please seek medical attention."
+            return "You are at: NO risk. Keep it up!"
+        
+print(check_blood_sugar(user_respond))    
+
+    
 
 
