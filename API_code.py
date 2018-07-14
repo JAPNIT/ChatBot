121eb755f3f4f6b00758b6bc34118a00b8b4e191
FUNS21 <39723057+FUNS21@users.noreply.github.com>
2018-07-14 08:35:11 +0800

    bayes lmao

--- API_code.py
+++ API_code.py
@@ -14,36 +14,36 @@ agent = Agent(
 def medical_records(medical):
         print ('bweh')
 	
-def saveColor(color):
+def summary(color):
 	print ('do something here')
 
 def createOrder(address):
 	print ('do something here')
 
 def main():
-	user_input = ''
+        user_input = ''
 
-	#loop the queries to API.AI so we can have a conversation client-side
-	while user_input != 'exit':
+        #loop the queries to API.AI so we can have a conversation client-side
+        while user_input != 'exit':
 
-		#parse the user input
-		user_input  = input("me: ")
-		#query the console with the user input, retrieve the response
-		response = agent.query(user_input)
-		#parse the response
-		result = response['result']
-		fulfillment = result['fulfillment']
-
-		#if an action is deteted, fire the appropriate function
-		if result['action'] == 'medical_records':
-                        print(Flag)
-                        medical_records(user_input)
-		if result['action'] == 'saveColor':
-			saveColor(user_input)
-		if result['action'] == 'createOrder':
-			createOrder(user_input)
-		
-		print('bot: ' + fulfillment['speech'])
+                #parse the user input
+                user_input  = input("me: ")
+                #query the console with the user input, retrieve the response
+                response = agent.query(user_input)
+                #parse the response
+                result = response['result']
+                print(result)
+                fulfillment = result['fulfillment']
+                
+                #if an action is deteted, fire the appropriate function
+                if result['action'] == 'medical_records':
+                        print(wah)
+                if fulfillment['speech'] == 'Yah sure lah':
+                        print("WAH")
+                if result['action'] == 'createOrder':
+                        createOrder(user_input)
+                
+                print('bot: ' + fulfillment['speech'])
 
 
 if __name__ == "__main__":
--- Extracting_Links.py
+++ Extracting_Links.py
@@ -1,5 +1,4 @@
-
-import urllib.request
+'''import urllib.request
 import sys
 from bs4 import BeautifulSoup
 
@@ -7,17 +6,43 @@ if __name__ == "__main__":
 
     ### Create opener with Google-friendly user agent
     opener = urllib.request.build_opener()
-    opener.addheaders = [('User-agent', 'GoogleChrome')]
+    opener.addheaders = [('User-agent', 'Google Chrome')]
 
     ### Open page & generate soup
     ### the "start" variable will be used to iterate through 10 pages.
-    for start in range(0,10):
-        url = "http://www.google.com/search?q=hello" + str(start*10)
-        page = opener.open(url)
+    for start in range(1):
+        url = "http://www.google.com/search?q=shrimp" + str(start)
+        page=opener.open(url)
         soup = BeautifulSoup(page, "html.parser")
 
         ### Parse and find
         ### Looks like google contains URLs in <cite> tags.
         ### So for each cite tag on each page (10), print its contents (url)
         for cite in soup.findAll('cite'):
-            print (cite.text)
+            print (cite.text)'''
+
+import re
+import os
+import sys
+import json
+ 
+from scrapy.spider import Spider
+from scrapy.selector import Selector
+ 
+class GoogleSearch(Spider):
+ 
+ #set the search result here
+    name = 'Google search'
+    allowed_domains = ['www.google.com']
+    start_urls = ['Insert the google url here']
+ 
+    def parse(self, response):
+ 
+        sel = Selector(response)
+        google_search_links_list = sel.xpath('//h3/a/@href').extract()
+        google_search_links_list = [re.search('q=(.*)&sa',n).group(1) for n in google_search_links_list]
+ 
+## Dump the output to json file
+        with open(output_j_fname, "w") as outfile:
+            json.dump({'output_url':google_search_links_list}, outfile, indent=4)
+
